03efd32a6c9253daaa4747192ee010701a9fc551
Preeti Sharma <preetisharma0123@gmail.com>
2024-07-30 03:06:09 -0300

    Add app.js, utils, dependencies: cross-env, eslint, cors, mongoose

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -1,1 +1,9 @@
-console.log('hello world')
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+console.log("hello, world")
+
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+})
