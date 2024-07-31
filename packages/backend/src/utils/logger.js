6cffb2d655800fb88b05eec3d11e0014611b4c9a
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2024-07-31 00:21:16 -0400

    chore: setup logger and middleware

---
+++ packages/backend/src/utils/logger.js
@@ -0,0 +1,42 @@
+const winston = require("winston");
+const appRoot = require("app-root-path");
+const path = require("path");
+
+const options = {
+  file: {
+    level: "info",
+    filename: path.join(appRoot.path, "logs", "app.log"),
+    handleExceptions: true,
+    maxsize: 5242880, // 5MB
+    maxFiles: 5,
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.json()
+    ),
+  },
+  console: {
+    level: "debug",
+    handleExceptions: true,
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.simple()
+    ),
+  },
+};
+
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.File(options.file),
+    new winston.transports.Console(options.console),
+  ],
+  exitOnError: false, // do not exit on handled exceptions
+});
+
+// Create a stream object with a 'write' function that will be used by `morgan`
+logger.stream = {
+  write: function (message, encoding) {
+    logger.info(message);
+  },
+};
+
+module.exports = logger;
