bd97cca1af0b466bfb742833c5c24deca7e38607
tiwariParth <243mjparth@gmail.com>
2024-08-10 21:13:20 +0530

    chore: Update Dockerfile and optimize build process for frontend deployment

--- packages/frontend/src/App.jsx
+++ packages/frontend/src/App.jsx
@@ -1,16 +1,52 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Signin from '../src/components/signin';
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Divider,
+  Link,
+  Image,
+} from "@nextui-org/react";
 
-const App = () => {
+import { useState } from "react";
+function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <>
-      <p>Where is my buddy?</p>
-      <Routes>
-        <Route path="/signin" element={<Signin />} />
-      </Routes>
-    </>
+    <div className="h-screen flex flex-col gap-10 justify-center items-center bg-gray-900">
+      <h1 className="text-4xl text-red-600">Where is my buddy</h1>
+      <Card className="max-w-[400px]">
+        <CardHeader className="flex gap-3">
+          <Image
+            alt="nextui logo"
+            height={40}
+            radius="sm"
+            src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
+            width={40}
+          />
+          <div className="flex flex-col">
+            <p className="text-md">NextUI</p>
+            <p className="text-small text-default-500">nextui.org</p>
+          </div>
+        </CardHeader>
+        <Divider />
+        <CardBody>
+          <p>Make beautiful websites regardless of your design experience.</p>
+        </CardBody>
+        <Divider />
+        <CardFooter>
+          <Link
+            isExternal
+            showAnchorIcon
+            href="https://github.com/nextui-org/nextui"
+          >
+            Visit source code on GitHub.
+          </Link>
+        </CardFooter>
+      </Card>
+    </div>
   );
-};
+}
 
+export default App;
 export default App;
--- packages/frontend/src/main.jsx
+++ packages/frontend/src/main.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { NextUIProvider } from "@nextui-org/react";
 
-// @ts-ignore
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <NextUIProvider>
       <App />
-    </BrowserRouter>
+    </NextUIProvider>
   </React.StrictMode>
 );
