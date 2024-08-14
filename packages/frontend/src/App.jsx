a44f4ffc66396fd66a3e919639ad2f5dfd526381
Vishal <vishalvarun27@gmail.com>
2024-08-15 00:31:05 +0530

    Configured Next UI and Added React Routing

--- packages/frontend/src/App.jsx
+++ packages/frontend/src/App.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Signin from './components/signin/signin';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,9 @@ function App() {
   return (
     <>
       <h1>Where is my buddy?</h1>
+      <Routes>
+        <Route path='/signin' element={<Signin />} />
+      </Routes>
     </>
   )
 }
--- packages/frontend/src/main.jsx
+++ packages/frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { NextUIProvider } from '@nextui-org/react';
+import { BrowserRouter } from 'react-router-dom';
 
+// @ts-ignore
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <NextUIProvider>
+        <App />
+      </NextUIProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
