f7894df091dab34910cf23b8ddcdd8853f142a1c
Vishal <vishalvarun27@gmail.com>
2024-08-15 02:13:21 +0530

    Fixed lint error

--- packages/frontend/src/App.jsx
+++ packages/frontend/src/App.jsx
@@ -3,14 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import Signin from '../src/components/signin';
 
 const App = () => {
-    return( 
+  return (
     <>
-        <p>Where is my buddy?</p>
-        <Routes>
-            <Route path='/signin' element={<Signin/>} />
-        </Routes>
+      <p>Where is my buddy?</p>
+      <Routes>
+        <Route path="/signin" element={<Signin />} />
+      </Routes>
     </>
-    )
+  );
 };
 
 export default App;
--- packages/frontend/src/components/signin.tsx
+++ packages/frontend/src/components/signin.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const signin = () => {
-  return (
-    <div>signin</div>
-  )
-}
+  return <div>signin</div>;
+};
 
-export default signin
+export default signin;
