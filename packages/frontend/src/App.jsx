e50b1e0da07e6f7a771be72b58bed7d499bc887c
Preeti Sharma <preetisharma0123@gmail.com>
2024-08-17 02:31:50 -0300

    fix: package.json, mongoose, vite.config

--- packages/frontend/src/App.jsx
+++ packages/frontend/src/App.jsx
@@ -1,52 +1,16 @@
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Divider,
-  Link,
-  Image,
-} from "@nextui-org/react";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Signin from './components/signin';
 
-import { useState } from "react";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="h-screen flex flex-col gap-10 justify-center items-center bg-gray-900">
-      <h1 className="text-4xl text-red-600">Where is my buddy</h1>
-      <Card className="max-w-[400px]">
-        <CardHeader className="flex gap-3">
-          <Image
-            alt="nextui logo"
-            height={40}
-            radius="sm"
-            src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
-            width={40}
-          />
-          <div className="flex flex-col">
-            <p className="text-md">NextUI</p>
-            <p className="text-small text-default-500">nextui.org</p>
-          </div>
-        </CardHeader>
-        <Divider />
-        <CardBody>
-          <p>Make beautiful websites regardless of your design experience.</p>
-        </CardBody>
-        <Divider />
-        <CardFooter>
-          <Link
-            isExternal
-            showAnchorIcon
-            href="https://github.com/nextui-org/nextui"
-          >
-            Visit source code on GitHub.
-          </Link>
-        </CardFooter>
-      </Card>
-    </div>
+    <Router>
+      <h1>Where is my buddy?</h1>
+      <Routes>
+        <Route path='/signin' element={<Signin />} />
+      </Routes>
+    </Router>
   );
 }
 
-export default App;
 export default App;
