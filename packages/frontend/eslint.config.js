e201fdbb5bc60411accdbf68608948e8ee62c028
Vishal <vishalvarun27@gmail.com>
2024-08-26 04:02:01 +0530

    fix: lintinting errors and husky pre-commit

--- packages/frontend/eslint.config.js
+++ packages/frontend/eslint.config.js
@@ -1,9 +1,11 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import pluginReact from 'eslint-plugin-react';
+import { version } from 'react';
 
 export default [
   { files: ['**/*.{js,mjs,cjs,jsx}'] },
+  { settings: { react: { version } } },
   { languageOptions: { globals: globals.browser } },
   { rules: { 'react/react-in-jsx-scope': 'off' } },
   pluginJs.configs.recommended,
--- packages/frontend/tailwind.config.js
+++ packages/frontend/tailwind.config.js
@@ -1,16 +1,17 @@
-// tailwind.config.js
-const { nextui } = require("@nextui-org/react");
+import { nextui } from '@nextui-org/react';
 
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+/ @type {import('tailwindcss').Config} */;
+const config = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src//.{js,ts,jsx,tsx}',
+    './node_modules/@nextui-org/theme/dist/**/.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {}
   },
-  darkMode: "class",
-  plugins: [nextui()],
+  darkMode: 'class',
+  plugins: [nextui()]
 };
+
+export default config;
