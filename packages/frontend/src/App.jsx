d88b239e53e61eeec17853c90c19ea30b9a433ad
Vishal <vishalvarun27@gmail.com>
2024-08-31 17:47:34 +0530

    feat[frontend]: sign-up page implemented

--- packages/frontend/src/App.jsx
+++ packages/frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signin from './pages/auth/signin';
 import ChangePassword from './pages/auth/change-password';
+import Signup from './pages/auth/signup';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/signin" element={<Signin />} />
         <Route path="/change-password" element={<ChangePassword />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   );
